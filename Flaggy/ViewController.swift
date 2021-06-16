9fd6a394cb5260be409266dfd7edbdeca8352f24
Terry <terrykuo501@gmail.com>
2021-06-16 21:52:07 +0800

    styling button

--- Flaggy/ViewController.swift
+++ Flaggy/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    var countries: [String] = ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+    var score = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureButton()
+        
+        askQuestion()
+    }
+    
+    func askQuestion() {
+        let number = Int.random(in: 0..<countries.count)
+        button1.setImage(UIImage(named: countries[number]), for: .normal)
+        
+        button2.setImage(UIImage(named: countries[number]), for: .normal)
+        button3.setImage(UIImage(named: countries[number]), for: .normal)
+    }
+    
+    func configureButton() {
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
     }
 
-
 }
+
 
